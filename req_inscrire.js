64815b70407888c61cffe2c72dc4736005db944b
DAFFIS <DAFFIS@intechS1.fr>
2017-11-20 14:16:18 +0100

    Ajout de fichier

---
+++ req_inscrire.js
@@ -0,0 +1,70 @@
+// req_inscrire
+
+"use strict";
+
+var fs = require("fs");
+require('remedial');
+
+var trait = function(req, res, query) {
+
+	var marqueurs;
+	var pseudo;
+	var password;
+	var page;
+	var membre;
+	var contenu_fichier;
+	var listeMembres;
+	var i; 
+	var trouve;
+
+	// ON LIT LES COMPTES EXISTANTS
+
+	contenu_fichier = fs.readFileSync("membre.json", 'utf-8');
+	listeMembre = JSON.parse(contenu_fichier);
+
+	// ON VERIFIE QUE LE COMPTE N'EXISTE PAS DEJA 
+
+	trouve = false;
+	i = 0;
+	while(i<listeMembre.length && trouve === false) {
+		if(listeMembres[i].pseudo === query.pseudo) {
+			trouve = true;
+		}
+		i++;
+	}
+
+	// SI PAS TROUVE, ON AJOUTE LE NOUVEAU COMPTE DANS LA LISTE DES COMPTES
+
+	if(trouve === false) {
+		nouveauMembre = {};
+		nouveauMembre.pseudo = query.pseudo;
+		nouveauMembre.password = query.password;
+		listeMembres[listeMembres.length] = nouveauMembre;
+
+		contenu_fichier = JSON.stringify(listeMembre);
+
+		fs.writeFileSync("membres.json", contenu_fichier, 'utf-8');
+	}
+
+	// ON RENVOIT UNE PAGE HTML
+
+	if(trouve === true) {
+		
+		// SI CREATION OK, ON ENVOIE PAGE DE CONFIRMATION 
+
+		page = fs.readFileSync('modele_confirmation_inscription.html', 'UTF-8');
+
+		marqueurs = {};
+		marqueurs.pseudo = query.pseudo;
+		marqueurs.password = query.password;
+		page = page.supplant(marqueurs);
+	}
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write(page);
+	res.end();
+};
+
+
+//-------------
+
+module.exports = trait;
