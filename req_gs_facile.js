ef6bf2592912843d93178f2341f67196cd17b157
DAFFIS <DAFFIS@intechS1.fr>
2017-11-28 10:18:51 +0100

    ajout de req standar et modif

---
+++ req_gs_facile.js
@@ -0,0 +1,23 @@
+// req grille standart facile // 
+
+"use strict";
+
+
+var fs = require("fs");
+require('remedial');
+
+var trait = function (req, res, query) {
+	
+	var liste = [];
+
+
+// ON CLIQUE SUR UNE CARTE POUR LA FAIRE PASSER EN VALEUR 1 OU 2,VALEUR 1 SIGNIFIE QU'ON CLIQUER SUR LA CARTE POUR LA COMBINER A UNE AUTRE ET LA FAIRE PASSER EN VALEUR DEUX, SI DEUX CARTES SONT PAREIL LES VALEUR RESTE A DEUX //
+
+
+
+
+	liste[1] = "0, 0, 0, 0"
+	liste[2] = "0, 0, 0, 0"
+    liste[3] = "0, 0, 0, 0"
+    liste[4] = "0, 0, 0, 0"
+
